baba97912590261a998172455d27d212258763aa
Khoadeptrai <khoa.soft.e@gmail.com>
2022-10-23 20:53:42 +0700

    Added FoodModel

---
+++ HotelManagerLibrary/Models/FoodModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagerLibrary.Models
+{
+    public class FoodModel
+    {
+        public int FoodID { get; set; }
+        public string FoodName { get; set; }
+        public Decimal FoodPrice { get; set; }
+        public byte[] FoodImage { get; set; }
+        public FoodModel()
+        {
+
+        }
+
+        public FoodModel(string foodName, decimal foodPrice, byte[] foodImage)
+        {
+            FoodName=foodName;
+            FoodPrice=foodPrice;
+            FoodImage=foodImage;
+        }
+    }
+}
